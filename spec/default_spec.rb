f3651468b5b86817784717cd1f42ab9d5bb13822
Jeff Smith <jsmith@rallydev.com>
2013-05-24 09:48:39 -0600

    Adding chef-spec tests to verify packages get installed

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,7 +2,13 @@ require 'chefspec'
 
 describe 'mail-client::default' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'mail-client::default' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
+
+  it 'should do install mutt' do
+    expect(chef_run).to install_package 'mutt'
   end
+
+  it 'should do install alpine' do
+    expect(chef_run).to install_package 'alpine'
+  end
+
 end

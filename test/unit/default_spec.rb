d2d4ce0c5f002fd368a3271d5eabf29c71c98352
Matt Farrar and Charlie Spear <mfarrar@rallydev.com>
2013-11-15 13:54:10 -0800

    Upgraded to chefspec3

--- test/unit/default_spec.rb
+++ test/unit/default_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 
 describe 'mail-client::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'mail-client::default' }
+  let (:chef_run) { ChefSpec::Runner.new.converge 'mail-client::default' }
 
   it 'should do install mutt' do
     expect(chef_run).to install_package 'mutt'

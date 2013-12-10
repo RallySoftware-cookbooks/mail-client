bf57928d13d8ad91396aa249fb390327fd579c41
Rally Dev Pair <pairing@rallydev.com>
2013-12-10 08:37:15 -0700

    Fixed unit tests and added ubuntu integration test

--- test/unit/default_spec.rb
+++ test/unit/default_spec.rb
@@ -1,14 +1,27 @@
 require_relative 'spec_helper'
 
 describe 'mail_client::default' do
-  let (:chef_run) { ChefSpec::Runner.new.converge 'mail_client::default' }
+  context 'when platform_family' do
 
-  it 'should do install mutt' do
-    expect(chef_run).to install_package 'mutt'
+    subject(:chef_run) do
+      ChefSpec::Runner.new do |node|
+        node.automatic_attrs[:platform_family] = platform_family
+      end.converge described_recipe
+    end
+
+    describe 'rhel' do
+      let(:platform_family) { 'rhel' }
+
+      it { should include_recipe 'yum::epel' }
+      it { should install_package 'mutt' }
+      it { should install_package 'alpine' }
+    end
+
+    describe 'debian' do
+      let(:platform_family) { 'debian' }
+
+      it { should include_recipe 'apt' }
+      it { should install_package 'mutt' }
+    end
   end
-
-  it 'should do install alpine' do
-    expect(chef_run).to install_package 'alpine'
-  end
-
 end
